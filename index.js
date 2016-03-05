38e2a9f1aa082834b78027578a16a831ccddf058
Murray Smith <murray@mismith.info>
2016-03-05 10:19:11 -0700

    switch from upload to process method (now stores metadata)

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 class FirebaseFileUploader {
-	constructor(ref) {
-		if ( ! (ref instanceof Firebase)) throw new Error('Firebase reference required');
-
-		this.ref = ref;
-	}
-	upload(files, single = false) {
+	process(files, ref, single = false) {
 		if ( ! files || ! files.length) return;
 
-		for(let i = 0; i < files.length; i++) {
-			let reader = new FileReader();
-			reader.onload = e => this.ref[single ? 'set' : 'push'](e.target.result);
-			reader.readAsDataURL(files[i]);
+		var processed = [];
 
-			if (single) break;
+		for(let i = 0; i < files.length; i++) {
+			processed.push(new Promise(resolve => {
+				let reader = new FileReader();
+				reader.onload = e => {
+					resolve({
+						name: files[i].name,
+						size: files[i].size,
+						type: files[i].type,
+						src: e.target.result,
+					});
+				};
+				reader.readAsDataURL(files[i]);
+			}));
 		}
+
+		return Promise.all(processed).then(files => {
+			if (ref) {
+				if (single) {
+					return ref.set(files[0] || null);
+				} else {
+					var deferreds = [];
+					files.map(file => deferreds.push(ref.push(file)));
+					return Promise.all(deferreds);
+				}
+			} else {
+				return files;
+			}
+		});
 	}
 }
