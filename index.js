6fb61cb423319d281a36860ea05b78bb36759dbc
Murray Smith <murray@businessinstincts.com>
2016-09-19 16:24:12 -0600

    add client-side resize function

--- index.js
+++ index.js
@@ -59,4 +59,59 @@ class FirebaseFileUploader {
 			}
 		});
 	}
+	resize(img, maxWidth, maxHeight, rotateDegrees) {
+		var imgWidth = img.width,
+			imgHeight = img.height,
+			ratio = Math.min(maxWidth / imgWidth, maxHeight / imgHeight), // Use the smallest ratio that the image best fit into the maxWidth x maxHeight box.
+			canvas = document.createElement('canvas'),
+			canvasCopy = document.createElement('canvas'),
+			canvasCopy2 = document.createElement('canvas'),
+			ctx = canvas.getContext('2d'),
+			copyCtx = canvasCopy.getContext('2d'),
+			copyCtx2 = canvasCopy2.getContext('2d');
+
+		// init
+		canvasCopy.width = canvasCopy2.width = imgWidth;
+		canvasCopy.height = canvasCopy2.height = imgHeight;
+		copyCtx.drawImage(img, 0, 0);
+		copyCtx2.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvasCopy2.width, canvasCopy2.height);
+
+		var rounds = 2,
+			roundRatio = ratio * rounds;
+		for (var i = 1; i <= rounds; i++) {
+			// tmp
+			canvasCopy.width = imgWidth * roundRatio / i;
+			canvasCopy.height = imgHeight * roundRatio / i;
+			copyCtx.drawImage(canvasCopy2, 0, 0, canvasCopy2.width, canvasCopy2.height, 0, 0, canvasCopy.width, canvasCopy.height);
+
+			// copy back
+			canvasCopy2.width = imgWidth * roundRatio / i;
+			canvasCopy2.height = imgHeight * roundRatio / i;
+			copyCtx2.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvasCopy2.width, canvasCopy2.height);
+		}
+
+		canvas.width = imgWidth * roundRatio / rounds;
+		canvas.height = imgHeight * roundRatio / rounds;
+		ctx.drawImage(canvasCopy2, 0, 0, canvasCopy2.width, canvasCopy2.height, 0, 0, canvas.width, canvas.height);
+
+		if (rotateDegrees) {
+			if (rotateDegrees == 90 || rotateDegrees == 270) {
+				canvas.width = canvasCopy2.height;
+				canvas.height = canvasCopy2.width;
+			} else {
+				canvas.width = canvasCopy2.width;
+				canvas.height = canvasCopy2.height;
+			}
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			if (rotateDegrees == 90 || rotateDegrees == 270) {
+				ctx.translate(canvasCopy2.height / 2, canvasCopy2.width / 2);
+			} else {
+				ctx.translate(canvasCopy2.width / 2, canvasCopy2.height / 2);
+			}
+			ctx.rotate(rotateDegrees * Math.PI / 180);
+			ctx.drawImage(canvasCopy2, -canvasCopy2.width / 2, -canvasCopy2.height / 2);
+		}
+
+		return canvas.toDataURL();
+	}
 }
